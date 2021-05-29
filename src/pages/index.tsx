8dc90c2792fdc73cc26f426d10f048cd272d3d2f
Marlon Andrei <marlon-master@hotmail.com>
2021-05-28 22:28:18 -0300

    First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Link from 'next/link';
+import Head from 'next/head';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +27,40 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Post | spacetraveling</title>
+      </Head>
+
+      <div className={`${styles.container} ${commonStyles.container}`}>
+        <img src="/logo.svg" alt="logo" />
+
+        <main className={styles.mainContent}>
+          {/* <Link href={#}>
+          <a key={}> */}
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos</p>
+          <div className={styles.infoContainer}>
+            <div>
+              <FiCalendar />
+              <small>28/05/2021</small>
+            </div>
+            <div>
+              <FiUser />
+              <small>Marlon Andrei</small>
+            </div>
+          </div>
+          {/*  </a>
+        </Link> */}
+        </main>
+
+        <a href="#">Carregar mais posts</a>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
